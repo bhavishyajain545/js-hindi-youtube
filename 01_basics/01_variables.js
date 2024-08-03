0570b0804c31bb6b3474d2b336b6ba768fe50f3a
bhavishyajain545 <65864699+bhavishyajain545@users.noreply.github.com>
2024-08-03 08:01:51 +0000

    new file added 02_variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 1445234
+let accountEmai = "jainbagvdiab.com"
+/* 
+prefer nort to use var 
+*/
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS  code as newer version
+//alert(3+3)//We are using nodejs not browser
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigInt    
+//string => ""
+// boolean => true/false
+// null => standalne value
+// undefined => 
+// symbol => unique
+
+//object 
+//null is object
+console.log(typeof null);
