64cd9fc3660d86371659abb144049cf6e96906fb
bhavishyajain545 <65864699+bhavishyajain545@users.noreply.github.com>
2024-08-03 08:23:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="33"
+
+let valueInNumber = Number(score)//new keyword Number to change anything to nukmber
+
+//console.log(typeof valueInNumber);
+//console.log(typeof (valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" =. false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof stringNumber);
